3e8c3c97c28ec4be475b3fc9ef21130abc0b4fe3
Ketchell T <Ketchell.terzakis@vtsd.com>
2024-01-11 11:46:07 -0500

    No

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,12 @@
 /* Put code for the task below */
+const button = document.getElementById("colorButton");
+
+var table = document.getElementById("table");
+function Changebgrd(){
+    //change the color of the table
+    table.style.backgroundColor = "red";
+
+    //change the color of the button
+    button.style.backgroundColor = "red";
+}
+}
