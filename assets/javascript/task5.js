4e781ad4655a81c71fb14c6bc21a36ef6c117cb8
Ketchell T <Ketchell.terzakis@vtsd.com>
2024-01-12 13:55:10 -0500

    No

--- assets/javascript/task5.js
+++ assets/javascript/task5.js
@@ -1,1 +1,24 @@
-/* Put code for the task below */
+
+const submit = document.getElementById("submit");
+const name = document.getElementById("fullName");
+const address = document.getElementById("address");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+// Add the event listener to the submit button
+submit.addEventListener("click", my_function);
+function my_function(){
+    // Check name field
+    if(fullName.value === ''){
+        fullName.style.backgroundColor = "red";
+    }else{
+        fullName.style.backgroundColor = 'white';
+    }
+
+    // Check Experience Field
+    if(address.value === ''){
+        address.style.backgroundColor = "red";
+    }else{
+        address.style.backgroundColor = 'white';
+    }
+
+
